a5722555e0784be6df4a407c352f81d19e46931f
PureAmaya <gkoishi0514@gmail.com>
2022-12-02 13:15:30 +0800

    稍微弄了点卡牌制作+添加了横幅通知

--- Assets/GameUI/CardMaker.cs
+++ Assets/GameUI/CardMaker.cs
@@ -29,6 +29,11 @@ public class CardMaker : MonoBehaviour
     /// 现在正在编辑的卡包的清单
     /// </summary>
     private CardBundlesManifest nowEditingBundle;
+
+        /// <summary>
+        /// 现在正在编辑的卡牌
+        /// </summary>
+    private CharacterCard nowEditingCard;
     
     /// <summary>
     /// 游戏加载时的实践
@@ -59,7 +64,7 @@ public class CardMaker : MonoBehaviour
         if (FileBrowser.CheckPermission() == FileBrowser.Permission.Denied)
         {
             //不能读取外部储存的有关逻辑
-            Notify.notify.CreateNotification(null, delegate { }, "储存权限被拒绝", "如果要编辑卡包，需要储存权限\n点击黑色区域返回游戏标题", 1f);
+            Notify.notify.CreateStrongNotification(null, delegate { }, "储存权限被拒绝", "如果要编辑卡包，需要储存权限\n点击黑色区域返回游戏标题", 1f);
         }
     }
     
@@ -76,9 +81,9 @@ public class CardMaker : MonoBehaviour
 /// <summary>
 /// 创建新卡牌
 /// </summary>
-public void CreateCard()
+public void CreateCard(bool onlyCard)
 {
-    
+    nowEditingCard = CardReadWrite.CreateNewCard(onlyCard);
 }
 
 
@@ -102,7 +107,7 @@ public void CreateCard()
     public void test()
     {
         //不能读取外部储存的有关逻辑
-        Notify.notify.CreateNotification(null, delegate { }, "储存权限被拒绝", "如果要编辑卡包，需要储存权限\n点击黑色区域返回游戏标题", 0.8f);
+    
     }
 #endif
 }
--- Assets/KitaujiGameDesignClub/GameFramework/UI/Notify.cs
+++ Assets/KitaujiGameDesignClub/GameFramework/UI/Notify.cs
@@ -2,6 +2,8 @@ using Lean.Gui;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
+
 
 namespace KitaujiGameDesignClub.GameFramework.UI
 {
@@ -10,37 +12,45 @@ namespace KitaujiGameDesignClub.GameFramework.UI
     {
         public static Notify notify = null;
 
+       
         /// <summary>
         /// 通知面板
         /// </summary>
-        [SerializeField] private LeanWindow Modal;
+        [FormerlySerializedAs("Modal")]
+        [Header("强通知")]
+        [SerializeField] private LeanWindow strongNotification;
 
         /// <summary>
         /// 通知内容
         /// </summary>
-        [SerializeField] private TMP_Text content;
+        [FormerlySerializedAs("content")] [SerializeField] private TMP_Text strongContent;
     
         /// <summary>
         /// 关闭通知
         /// </summary>
         [SerializeField] private LeanButton Shutdown;
 
+        
+        [Header("普通通知")] [SerializeField] private LeanPulse BannerNotification;
+        [SerializeField] private TMP_Text BannerContent;
+
         /// <summary>
-        /// 默认字体大小
+        /// 默认字体大小0=Banner 1=strong
         /// </summary>
-        private float DefaultfontSize;
+        private float[] DefaultfontSize = new float[2];
     
         private void Awake()
         {
             //关闭面板
-            Modal.TurnOff();
-            content.text = string.Empty;
+            strongNotification.TurnOff();
+            strongContent.text = string.Empty;
             //清除事件
             Shutdown.OnDown.RemoveAllListeners();
-            Modal.OnOn.RemoveAllListeners();
+            strongNotification.OnOn.RemoveAllListeners();
             //缓存字体大小
-            DefaultfontSize = content.fontSize;
-
+            DefaultfontSize[1] = strongContent.fontSize;
+            DefaultfontSize[0] = BannerContent.fontSize;
+            
             //不能存在多个
             var notificationCount = GameObject.FindObjectsOfType(typeof(Notify)).Length;
             if (notificationCount > 1)
@@ -54,30 +64,71 @@ namespace KitaujiGameDesignClub.GameFramework.UI
         }
 
         /// <summary>
-        /// 创建一个通知
+        /// 创建强通知（弹窗通知）
         /// </summary>
-        /// <param name="OnNotify">通知开始时的事件</param>
+        /// <param name="onNotify">通知开始时的事件</param>
         /// <param name="OnOff">通知关闭后的事件</param>
         /// <param name="title">通知标题</param>
         /// <param name="content">通知内容</param>
-        /// <param name="fontSize">全局字体大小（相对值）</param>
-        public void CreateNotification(UnityAction OnNotify,UnityAction OnOff,string title,string content,float fontSizeRate = 1f)
+        /// <param name="fontSizeRate">全局字体大小（相对值）</param>
+        public void CreateStrongNotification(UnityAction onNotify,UnityAction OnOff,string title,string content,float fontSizeRate = 1f)
         {
-            if(OnNotify != null)  Modal.OnOn.AddListener(OnNotify);
+            if(onNotify != null)  strongNotification.OnOn.AddListener(onNotify);
        
             Shutdown.OnDown.AddListener(delegate
             {
-                Modal.TurnOff();
+                strongNotification.TurnOff();
                 if(OnOff != null) OnOff.Invoke();
             });
 
-            this.content.text = $"<size=132%><align=\"center\">{title}</align></size>\n\n{content}";
-            this.content.fontSize = DefaultfontSize * fontSizeRate;
+            this.strongContent.text = $"<size=132%><align=\"center\">{title}</align></size>\n\n{content}";
+            this.strongContent.fontSize = DefaultfontSize[1] * fontSizeRate;
         
-            Modal.TurnOn();
-            Debug.Log($"发生通知：{title} - {content}");
+            strongNotification.TurnOn();
+            Debug.Log($"发生强通知：{title} - {content}");
      
       
         }
+
+/// <summary>
+/// 创建通知（横幅通知）
+/// </summary>
+/// <param name="onNotify">通知开始时的事件</param>
+/// <param name="content">通知内容</param>
+/// <param name="fontSizeRate">全局字体大小（相对值）</param>
+        public void CreateBannerNotification(UnityAction onNotify, string content,float fontSizeRate = 1f)
+        {
+            if(onNotify != null)  BannerNotification.OnPulse.AddListener(delegate(int arg0) { onNotify.Invoke(); });
+
+         
+
+            this.strongContent.text = content;
+            this.strongContent.fontSize = DefaultfontSize[0] * fontSizeRate;
+        
+            BannerNotification.Pulse();
+            Debug.Log($"发生通知：{content}");
+        }
+
+
+#if UNITY_EDITOR
+        /// <summary>
+        /// 测试横幅通知
+        /// </summary>
+        [ContextMenu("test banner notification")]
+        public void TestBanner()
+        {
+            CreateBannerNotification(null,"测试通知");
+        }
+        
+        
+        /// <summary>
+        /// 测试强通知
+        /// </summary>
+        [ContextMenu("test strong notification")]
+        public void TestStrong()
+        {
+           CreateStrongNotification(null,null,"测试通知","1145141919810");
+        }
+#endif
     }
 }
